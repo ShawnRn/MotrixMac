066b701877af6f10f2616fd8ee7ef07029a1d483
ShawnRn <shawnrain.me@gmail.com>
2026-01-22 09:44:00 +0800

    Initial Commit

---
+++ MotrixMac/MotrixMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotrixMacApp.swift
+//  MotrixMac
+//
+//  Created by Shawn Rain on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MotrixMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
